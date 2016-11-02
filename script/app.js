7628e86376194cca1470e6817c376259dfdd7280
Eve Papineau <eve.papineau@hotmail.fr>
2016-11-02 20:11:08 +0000

    added answers

--- script/app.js
+++ script/app.js
@@ -1,1 +1,13 @@
 /* Write your code here! */
+var $replace = $("<button id='replace' class='myButton'>show announcement</button>")
+var $button = $("<button id='btn' class='myButton'>X</button>")
+$(".announcement").append($button);
+$(".announcement").append($replace);
+
+// $("button").click(function(){
+//     $(this).hide();
+// })
+$(".myButton").click(function(event){
+    $(".announcement p, #replace, #btn").toggle();
+})
+
